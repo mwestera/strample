93e1a993663200a742c9b7bf08c3b88c4a731980
mwestera <m.westera@hum.leidenuniv.nl>
2024-07-24 14:08:51 +0200

    add --span argument

--- strample.py
+++ strample.py
@@ -8,6 +8,7 @@ import webbrowser
 import tempfile
 from typing import List, Union
 import random
+import functools
 
 """
 
@@ -38,7 +39,11 @@ def parse_arguments():
     parser.add_argument('--seed', type=int, default=None, help='Random seed.')
     parser.add_argument('--out', type=argparse.FileType('w'), default=None, help='Output file for .html; otherwise served in webbrowser.')
     parser.add_argument('--descending', action='store_true', required=False, help='To show samples in descending order.')
+    parser.add_argument('-s', '--span', type=str, required=False, help='To highlight spans, a csv triple of field names: text,start,end')
     args = parser.parse_args()
+    if args.span:
+        key, start, end = args.span.split(',')
+        args.span = (key, start, end)
     return args
 
 
@@ -52,12 +57,18 @@ def main():
     logging.info(f'Random seed: {args.seed}')
     np.random.seed(args.seed)
 
+    make_html = functools.partial(generate_html,
+                                  sample_size=args.sample_size,
+                                  num_quantiles=args.quantiles,
+                                  key=args.key,
+                                  ascending=not args.descending,
+                                  span=args.span)
     data = pd.read_csv(args.csv)
 
     if args.key is None:
         args.key = get_first_numerical_column(data)
 
-    html_output = generate_html(data, args.sample_size, args.quantiles, args.key, not args.descending)
+    html_output = make_html(data)
 
     if args.out:
         args.out.write(html_output)
@@ -76,49 +87,59 @@ def get_first_numerical_column(data: pd.DataFrame) -> Union[None, int]:
     return numerical_columns[0]
 
 
-def bottom_html(data: pd.DataFrame, sample_size: int, key: str) -> List[str]:
+def bottom_html(data: pd.DataFrame, sample_size: int, key: str, span: tuple) -> List[str]:
+    sample = data.nsmallest(sample_size, key).sort_values(by=key)
     html = [
         f"<h2>Bottom {sample_size} rows</h2>",
-        data.nsmallest(sample_size, key).sort_values(by=key).to_html(index=False)
+        sample_to_html(sample, span)
     ]
     return html
 
 
-def top_html(data: pd.DataFrame, sample_size: int, key: str) -> List[str]:
+def top_html(data: pd.DataFrame, sample_size: int, key: str, span: tuple) -> List[str]:
+    sample = data.nlargest(sample_size, key).sort_values(by=key)
     html = [
         f"<h2>Top {sample_size} rows</h2>",
-        data.nlargest(sample_size, key).sort_values(by=key).to_html(index=False)
+        sample_to_html(sample, span),
     ]
     return html
 
 
-def quantiles_html(data: pd.DataFrame, num_quantiles: int, sample_size: int, key: str, ascending: bool) -> List[str]:
+def quantiles_html(data: pd.DataFrame, num_quantiles: int, sample_size: int, key: str, ascending: bool, span: tuple) -> List[str]:
     html = []
 
     quantiles = np.linspace(0, 1, num_quantiles + 1)
     for i in range(num_quantiles) if ascending else range(num_quantiles-1, -1, -1):
         q_start = quantiles[i]
         q_end = quantiles[i + 1]
-        quantile_data = data[
-            (data[key] > data[key].quantile(q_start)) & (data[key] <= data[key].quantile(q_end))]
-        sample_data = quantile_data.sample(min(sample_size, len(quantile_data)), random_state=1).sort_values(by=key)
+        quantile_data = data[(data[key] > data[key].quantile(q_start)) & (data[key] <= data[key].quantile(q_end))]
+        sample = quantile_data.sample(min(sample_size, len(quantile_data)), random_state=1).sort_values(by=key)
         if ascending:
             html.append(f"<h2>Quantile {q_start * 100:.0f}-{q_end * 100:.0f}%</h2>")
         else:
             html.append(f"<h2>Quantile {q_end * 100:.0f}-{q_start * 100:.0f}%</h2>")
-        html.append(sample_data.to_html(index=False))
+
+        html.append(sample_to_html(sample, span))
 
     return html
 
 
-def generate_html(data: pd.DataFrame, sample_size: int, num_quantiles: int, key: str, ascending: bool = True):
+def sample_to_html(sample: pd.DataFrame, span: tuple):
+    text_column, start_column, end_column = span
+    sample[text_column] = [f'{t[:start]}<span style="background-color:#ff0;">{t[start:end]}</span>{t[end:]}' for t, start, end in zip(sample[text_column], sample[start_column], sample[end_column])]
+
+    html = sample.to_html(index=False).replace('&lt;', '<').replace('&gt;', '>')
+    return html
+
+
+def generate_html(data: pd.DataFrame, sample_size: int, num_quantiles: int, key: str, ascending: bool = True, span: tuple = None):
     html = ["<html><body>"]
 
-    html.extend(bottom_html(data, sample_size, key) if ascending else top_html(data, sample_size, key))
-
-    html.extend(quantiles_html(data, num_quantiles, sample_size, key, ascending))
-
-    html.extend(top_html(data, sample_size, key) if ascending else bottom_html(data, sample_size, key))
+    bottom = bottom_html(data, sample_size, key, span)
+    top = top_html(data, sample_size, key, span)
+    html.extend(bottom if ascending else top)
+    html.extend(quantiles_html(data, num_quantiles, sample_size, key, ascending, span))
+    html.extend(top if ascending else bottom)
 
     html.append("</body></html>")
     return "".join(html)
